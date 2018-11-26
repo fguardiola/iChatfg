ab9074b0e84031d595f05b1f92339b8d7a532ac7
fguardiola <fernando.guardiola.888@gmail.com>
2018-11-26 11:14:15 +0100

    First commit

---
+++ iChatfg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChatfg
+//
+//  Created by 67621177 on 26/11/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
